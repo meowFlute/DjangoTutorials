034a1654bfb1a7f9bde8e8be2eb7104bc4ea5034
M. Scott Christensen <mscottchristensen@gmail.com>
2016-01-03 00:14:40 -0700

    Completed part 4 of the tutorial, including using generic views to route generic data to different templates

--- Poll App/polls/urls.py
+++ Poll App/polls/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     #example /polls/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     #example /polls/1/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     #example /polls/1/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     #example /polls/1/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
